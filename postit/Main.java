28f59f3b76ab601bb94fb7857ec0281f4337777f
Aneta <137305034+Aneta06@users.noreply.github.com>
2023-10-03 12:38:35 +0200

    postit

---
+++ postit/Main.java
@@ -0,0 +1,21 @@
+package postit;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        Create a few example post-it objects:
+           an orange with blue text: "Idea 1"
+        a pink with black text: "Awesome"
+            a yellow with green text: "Superb!"
+         */
+
+        PostIt one = new PostIt("orange", "Idea 1", "blue");
+        PostIt two = new PostIt("pin", "Awesome", "black");
+        PostIt three = new PostIt("yellow","Superb!","green");
+
+
+
+
+    }
+}
