0142e45dddb17fbfba81e888f7ba994cfda70234
Aneta <137305034+Aneta06@users.noreply.github.com>
2023-10-03 12:37:22 +0200

    blogpost

---
+++ blogpost/BlogPost.java
@@ -0,0 +1,24 @@
+package blogpost;
+
+public class BlogPost {
+    //Create a BlogPost class that has
+    //an authorName
+    //a title
+    //a text
+    //a publicationDate (string)
+
+   private String authorName;
+   private String title;
+   private String text;
+   private String publicationDate;
+
+   public BlogPost (String authorName, String title, String text, String publicationDate){
+
+       this.authorName = authorName;
+       this.title = title;
+       this.text = text;
+       this.publicationDate = publicationDate;
+   }
+
+}
+
